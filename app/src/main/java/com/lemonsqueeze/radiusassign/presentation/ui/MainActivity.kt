fe71d3098eb4aa7fa0765628e2d8ce64b93b7424
manishpandeyvp <manishpandeyvp@gmail.com>
2023-06-28 17:26:23 +0530

    Initial Libraries Setup

--- app/src/main/java/com/lemonsqueeze/radiusassign/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/lemonsqueeze/radiusassign/presentation/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.lemonsqueeze.radiusassign
+package com.lemonsqueeze.radiusassign.presentation.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.lemonsqueeze.radiusassign.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
